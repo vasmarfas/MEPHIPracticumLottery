107068f176c5edc1044dc079c91373bd115efacd
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-04-17 18:16:41 -0300

    feature(lottery): ticket model + repo

---
+++ src/main/java/org/mephi_kotlin_band/lottery/features/payment/controller/model/Ticket.java
@@ -0,0 +1,53 @@
+package org.mephi_kotlin_band.lottery.features.lottery.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.mephi_kotlin_band.lottery.features.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Ticket {
+
+    public enum Status {
+        PENDING,
+        WIN,
+        LOSE,
+        INVALID
+    }
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "draw_id", nullable = false)
+    private Draw draw;
+
+    @Column(nullable = false)
+    private String numbers;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+} 
