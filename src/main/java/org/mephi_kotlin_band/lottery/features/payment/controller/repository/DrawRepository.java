71434d20496f5dd15cdc221190213e7b2901bd0f
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-04-17 18:22:35 -0300

    feature(lottery): draw models + repos

---
+++ src/main/java/org/mephi_kotlin_band/lottery/features/payment/controller/repository/DrawRepository.java
@@ -0,0 +1,16 @@
+package org.mephi_kotlin_band.lottery.features.lottery.repository;
+
+import org.mephi_kotlin_band.lottery.features.lottery.model.Draw;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface DrawRepository extends JpaRepository<Draw, UUID> {
+    List<Draw> findByStatus(Draw.Status status);
+    List<Draw> findByStartTimeBefore(LocalDateTime time);
+    List<Draw> findByStartTimeBeforeAndStatus(LocalDateTime time, Draw.Status status);
+} 
