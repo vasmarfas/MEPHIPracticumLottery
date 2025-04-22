02f2dcf4f241301d398ec210b35b87e6b2499700
meeweel <meeweel@yandex.ru>
2025-04-23 01:30:41 +0300

    LOT-71 Services

---
+++ src/main/java/org/mephi_kotlin_band/lottery/features/lottery/service/DrawService.java
@@ -0,0 +1,16 @@
+package org.mephi_kotlin_band.lottery.features.lottery.service;
+
+import org.mephi_kotlin_band.lottery.features.lottery.dto.CreateDrawRequest;
+import org.mephi_kotlin_band.lottery.features.lottery.dto.DrawDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface DrawService {
+    DrawDto createDraw(CreateDrawRequest request);
+    List<DrawDto> getAllDraws();
+    List<DrawDto> getActiveDraws();
+    DrawDto getDrawById(UUID id);
+    DrawDto cancelDraw(UUID id);
+    void updateDrawStatuses();
+}
