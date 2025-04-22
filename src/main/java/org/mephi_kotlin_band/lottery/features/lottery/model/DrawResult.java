6d43e6a8e6c3fac2e0b27f9f1315a9ddd22a1954
meeweel <meeweel@yandex.ru>
2025-04-23 01:29:40 +0300

    LOT-71 Models

---
+++ src/main/java/org/mephi_kotlin_band/lottery/features/lottery/model/DrawResult.java
@@ -0,0 +1,32 @@
+package org.mephi_kotlin_band.lottery.features.lottery.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "draw_results")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class DrawResult {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "draw_id", nullable = false)
+    private Draw draw;
+
+    @Column(nullable = false)
+    private String winningCombination;
+
+    @Column(nullable = false)
+    private LocalDateTime resultTime;
+}
